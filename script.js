592ca78e448152bc5649e218fe07efd8cd33e3ba
GhostRiderGaming <hv90507@gmail.com>
2025-03-22 06:53:12 +0000

    api fixes

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ function startFlirting() {
   let inputValue = "Hello, how are you?"; // Insert input value here
 
 fetch(
-  "http://localhost:3000/proxy",
+  
+  "https://astra.datastax.com/api/v1/run/b8b371be-e5bc-46f9-a46a-d3f6193c8b80?stream=false",
   {
     method: "POST",
     headers: {
-      "Authorization": "AstraCS:WekjSYmdIrKxyFBXOpOtZdoH:ae8fcced8a73507e9a5dc746c4b98152023141e8a13d7932eabc7c2ac81245f9",  // Replace with your token
+      "Authorization": "AstraCS:TCvgwvJZkImFJaUpSJFCckCs:d487ea7fa70407eabe8c2afb6d235b641cb563cb58a2f284dac4a49455a98fa7",  // Replace with your token
       "Content-Type": "application/json",
       "x-api-key": "YOUR_API_KEY_HERE",  // Replace with your API key
     },
